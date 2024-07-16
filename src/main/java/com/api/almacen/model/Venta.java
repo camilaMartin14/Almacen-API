591fee28889bcec33655be4c0567e394219875d1
camilaMartin14 <camilamartin1403@gmail.com>
2024-07-16 08:06:51 -0300

    terminar CRUD modelo Venta

--- src/main/java/com/api/almacen/model/Venta.java
+++ src/main/java/com/api/almacen/model/Venta.java
@@ -1,15 +1,34 @@
 package com.api.almacen.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import java.time.LocalDate;
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter@Setter
 @Entity
 public class Venta {
+    @Id
+    @GeneratedValue(strategy=GenerationType.SEQUENCE)
     private Long codigo_venta;
     private LocalDate fecha_venta;
     private double total;
+    private List<Producto> listaProductos;
+    private Cliente cliente;
+
+    public Venta() {
+    }
+
+    public Venta(Long codigo_venta, LocalDate fecha_venta, double total, List<Producto> listaProductos, Cliente cliente) {
+        this.codigo_venta = codigo_venta;
+        this.fecha_venta = fecha_venta;
+        this.total = total;
+        this.listaProductos = listaProductos;
+        this.cliente = cliente;
+    }
     
 }
--- src/main/java/com/api/almacen/service/VentaService.java
+++ src/main/java/com/api/almacen/service/VentaService.java
@@ -1,10 +1,50 @@
 package com.api.almacen.service;
 
+import com.api.almacen.model.Venta;
 import com.api.almacen.repository.IVentaRepository;
+import java.time.LocalDate;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class VentaService implements IVentaService{
     
     @Autowired
     private IVentaRepository venRepo;
+
+    @Override
+    public List<Venta> getVentas() {
+        List <Venta> listaVentas= venRepo.findAll();
+        return listaVentas;
+    }
+
+    @Override
+    public Venta findVenta(Long id) {
+        Venta ven = venRepo.findById(id).orElse(null);
+        return ven;
+    }
+
+    @Override
+    public void saveVenta(Venta ven) {
+        venRepo.save(ven);
+    }
+
+    @Override
+    public void deleteVenta(Long id) {
+        venRepo.deleteById(id);
+    }
+
+    @Override
+    public void editVenta(Long codigoVentaOriginal,
+                            Long nuevoCodigoVenta, 
+                            LocalDate nuevaFechaVenta, 
+                            double nuevoTotal) {
+        
+        Venta ven = this.findVenta(codigoVentaOriginal);
+        
+        ven.setCodigo_venta(nuevoCodigoVenta);
+        ven.setFecha_venta(nuevaFechaVenta);
+        ven.setTotal(nuevoTotal);
+        
+        this.saveVenta(ven);
+    }
 }
